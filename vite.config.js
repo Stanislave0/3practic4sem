b95a6ce601e343fba98b02119814e4c013998539
Stanislave0 <stassot67@gmail.com>
2025-06-04 00:16:51 +0300

    fixed basename

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/3practic3sem",
+  base: "/3practic3sem/tree/gh-pages",
   plugins: [react()],
 })
